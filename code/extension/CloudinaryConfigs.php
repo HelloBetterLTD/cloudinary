135da2817eabe3c29c95e460f07a1691f782b493
Nivanka Fonseka <nivanka@silverstripers.com>
2016-04-11 20:38:46 +0530

    added basic model classes

---
+++ code/extension/CloudinaryConfigs.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Created by Nivanka Fonseka (nivanka@silverstripers.com).
+ * User: nivankafonseka
+ * Date: 4/11/16
+ * Time: 8:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class CloudinaryConfigs extends DataExtension
+{
+
+	private static $db = array(
+		'CloudName'				=> 'Varchar(100)',
+		'APIKey'				=> 'Varchar(100)',
+		'APISecret'				=> 'Varchar(100)'
+	);
+
+	public function updateCMSFields(FieldList $fields)
+	{
+		$fields->addFieldsToTab('Root.Settings.Cloudinary', array(
+			TextField::create('CloudName'),
+			TextField::create('APIKey'),
+			TextField::create('APISecret')
+		));
+	}
+
+}
---
+++ code/model/CloudinaryFile.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by Nivanka Fonseka (nivanka@silverstripers.com).
+ * User: nivankafonseka
+ * Date: 4/11/16
+ * Time: 8:36 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class CloudinaryFile extends DataObject
+{
+
+	private static $db = array(
+		'PublicID'				=> 'Varchar(500)'
+	);
+
+}
